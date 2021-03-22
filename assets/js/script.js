8003e97c0bfc7ff217097b6ebe192ae645e0b9fb
kerekmarci <kerekmarci@gmail.com>
2021-03-22 21:27:04 +0000

    Toggle changes background color to light

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 // ******* DARK/LIGHT THEME TOGGLE  *******
 
 const themeToggle = document.getElementById('ThemeCheckbox');
+//const text = document.querySelectorAll('p');
 themeToggle.addEventListener('change', () => {
     document.body.classList.toggle('light');
-});
+    document.body.children[0].classList.toggle('light'); 
+    
+})
