902a149f48d766d949f8eb6eee0b19cf1aa86ccf
kerekmarci <kerekmarci@gmail.com>
2021-03-23 14:26:05 +0000

    Theme toggle applied on all pages and root colour codes rearranged

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,31 @@
 // ******* DARK/LIGHT THEME TOGGLE  *******
-let lightMode = localStorage.getItem('lightMode');
-const themeToggle = document.getElementById('ThemeCheckbox');
 
+const themeToggle = document.getElementById('ThemeCheckbox');
+const currentTheme = localStorage.getItem('data-theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    
+    if (currentTheme === 'dark') {
+        themeToggle.checked = false;
+    } else {
+        themeToggle.checked = true;
+    }
+}  
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('data-theme', 'light');  
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('data-theme', 'dark'); 
+    }
+} 
+
+themeToggle.addEventListener('change', switchTheme, false);
+
+/*
 const enableLightMode = () => {
     document.body.classList.add('light'); 
     localStorage.setItem('lightMode', 'enabled');
@@ -26,4 +50,5 @@ themeToggle.addEventListener('change', () => {
         disableLightMode();
         console.log('Dark mode enabled');
     }
-})
+})*/
+
