c74a6fef06a18f9532ad7ad6db9209d68a113eac
kerekmarci <kerekmarci@gmail.com>
2021-03-23 06:21:42 +0000

    Light theme toggle will update local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,24 @@
 // ******* DARK/LIGHT THEME TOGGLE  *******
-
+let lightMode = localStorage.getItem('lightMode');
 const themeToggle = document.getElementById('ThemeCheckbox');
+
+const enableLightMode = () => {
+    document.body.classList.add('light'); 
+    localStorage.setItem('lightMode', 'enabled');
+}
+
+const disableLightMode = () => {
+    document.body.classList.remove('light'); 
+    localStorage.setItem('lightMode', 'disabled');
+}
+
 themeToggle.addEventListener('change', () => {
-    document.body.classList.toggle('light');  
+    lightMode = localStorage.getItem('lightMode');  
+    if (lightMode !== 'enabled') {
+        enableLightMode();
+        console.log('Light mode enabled');
+    } else {
+        disableLightMode();
+        console.log('Dark mode enabled');
+    }
 })
