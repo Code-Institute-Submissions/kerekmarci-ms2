0d177ba3fe7b7b357dc6727459033edb95368687
kerekmarci <kerekmarci@gmail.com>
2021-03-23 05:56:39 +0000

    Theme toggle improved: colour selections extended for light theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,6 @@
 // ******* DARK/LIGHT THEME TOGGLE  *******
 
 const themeToggle = document.getElementById('ThemeCheckbox');
-//const text = document.querySelectorAll('p');
 themeToggle.addEventListener('change', () => {
-    document.body.classList.toggle('light');
-    document.body.children[0].classList.toggle('light'); 
-    
+    document.body.classList.toggle('light');  
 })
